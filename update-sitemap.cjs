0b3a8d13eba0b78dbce805769e03acdec052aa91
mysamargroup <mysamargroup@gmail.com>
2025-10-18 18:13:01 +0000

    fix: Update sitemap in dist directory

--- update-sitemap.cjs
+++ update-sitemap.cjs
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const sitemapPath = path.resolve(__dirname, 'public', 'sitemap.xml');
+const distSitemapPath = path.resolve(__dirname, 'dist', 'sitemap.xml');
 
 fs.readFile(sitemapPath, 'utf8', (err, data) => {
   if (err) {
@@ -12,7 +13,7 @@ fs.readFile(sitemapPath, 'utf8', (err, data) => {
   const today = new Date().toISOString().split('T')[0];
   const updatedSitemap = data.replace(/<lastmod>.*<\/lastmod>/, `<lastmod>${today}</lastmod>`);
 
-  fs.writeFile(sitemapPath, updatedSitemap, 'utf8', (err) => {
+  fs.writeFile(distSitemapPath, updatedSitemap, 'utf8', (err) => {
     if (err) {
       console.error('Error writing sitemap.xml:', err);
       return;
